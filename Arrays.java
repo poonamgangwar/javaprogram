5341a495fcff421997c2400d283812c29a95d147
poonam kumari <53055667+poonamgangwar@users.noreply.github.com>
2024-07-24 14:13:02 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Arrays {
+    public static void main(String args[]){
+        int[] marks = new int[3];
+        marks[0] = 97; //phys
+        marks[1] = 98; //chem
+        marks[2] = 95; //eng
+        //System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+
+        for(int i=0; i<3; i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
+
