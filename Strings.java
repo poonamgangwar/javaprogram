2feb469828b6ab3066e784ee8ab0d2804821d42a
poonam kumari <53055667+poonamgangwar@users.noreply.github.com>
2024-07-25 11:10:01 +0530

    Add files via upload

--- Strings.java
+++ Strings.java
@@ -1,34 +1,9 @@
-import java.util.*;
+package Strings;
 
+public class Strings {
+    public static void main(String args[]) {
+        String name = "TonyStark";
 
-    public class Strings {
-        public static void main(String args[]) {
-            String name1 = "Tony";
-            String name2 = "Tony";
-
-
-            if(name1.equals(name2)) {
-                System.out.println("They are the same string");
-            } else {
-                System.out.println("They are different strings");
-            }
-
-
-            //DO NOT USE == to check for string equality
-            //Gives correct answer here
-            if(name1 == name2) {
-                System.out.println("They are the same string");
-            } else {
-                System.out.println("They are different strings");
-            }
-
-
-            //Gives incorrect answer here
-            if(new String("Tony") == new String("Tony")) {
-                System.out.println("They are the same string");
-            } else {
-                System.out.println("They are different strings");
-            }
-
-        }
+        System.out.println(name.substring(0, 4));
     }
+}
